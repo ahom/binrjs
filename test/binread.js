dd1f93b468214d899dc146d94ad42df2f22f7e6f
Antoine Hom <antoine.hom@gmail.com>
2015-02-07 13:03:26 +0100

    Return stacktraces and value when calling the trace_read APIs

--- test/binread.js
+++ test/binread.js
@@ -24,25 +24,31 @@ describe('binread', function () {
 
   it('.trace_read', function (done) {
     async_test(binread.trace_read(types.int8, [0x01]), done, function (value) {
-      assert.equal(value.length, 1);
+      assert.equal(value.value, 0x01);
 
-      var stack_trace = value[0];
+      var stack_traces = value.stack_traces;
+      assert.equal(stack_traces.length, 1);
+
+      var stack_trace = stack_traces[0];
       assert.equal(stack_trace.offset, 0);
       assert.equal(stack_trace.size, 1);
-      assert.equal(stack_trace.value, 0x01);
+      assert.equal(stack_trace.value, value.value);
       assert.equal(stack_trace.type.func, types.int8);
     });
   });
 
   it('.trace_read_with_args', function (done) {
     async_test(binread.trace_read_with_args(types.bytes, [0x01, 0x02])(2), done, function (value) {
-      assert.equal(value.length, 1);
+      assert.equal(value.value[0], 0x01);
+      assert.equal(value.value[1], 0x02);
 
-      var stack_trace = value[0];
+      var stack_traces = value.stack_traces;
+      assert.equal(stack_traces.length, 1);
+
+      var stack_trace = stack_traces[0];
       assert.equal(stack_trace.offset, 0);
       assert.equal(stack_trace.size, 2);
-      assert.equal(stack_trace.value[0], 0x01);
-      assert.equal(stack_trace.value[1], 0x02);
+      assert.equal(stack_trace.value, value.value);
       assert.equal(stack_trace.type.func, types.bytes);
       assert.equal(stack_trace.type.args[0], 2);
     });
