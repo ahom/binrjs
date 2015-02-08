8a3402baeb7528a6a30ce938fdf928229ebb5478
Antoine Hom <antoine.hom@gmail.com>
2015-02-08 16:52:59 +0100

    Remove unneeded test

--- test/binread.js
+++ test/binread.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var assert = require('assert');
+var fs = require('fs');
 var binread = require('../lib/binread');
 var utils = require('./utils');
 var async_test = utils.async_test;
@@ -32,149 +33,162 @@ var test_result = {
     minor: 1
   }
 };
-var test_data = [
+var test_data_array = [
   0x01, 0x02, 0x03, 0x04, // magic
   0x10, 0x00,             // major
   0x00, 0x01              // minor
 ];
 
 describe('binread', function () {
-  it('read', function (done) {
-    async_test(binread.read(test_struct, test_data), done, function (value) {
-      assert.deepEqual(value.magic, test_result.magic);
+  var tests = function (test_data, data_type_string) {
+    describe(data_type_string, function () {
+      it('read', function (done) {
+        async_test(binread.read(test_struct, test_data), done, function (value) {
+          assert.deepEqual(value.magic, test_result.magic);
 
-      async_test(value.version.read(), done, function (version_value) {
-        assert.deepEqual(version_value, test_result.version);
+          async_test(value.version.read(), done, function (version_value) {
+            assert.deepEqual(version_value, test_result.version);
+          });
+        }, true);
       });
-    }, true);
-  });
 
-  it('read_with_args', function (done) {
-    async_test(binread.read_with_args(test_struct, test_data)(1, 2), done, function (value) {
-      assert.deepEqual(value.magic, test_result.magic);
+      it('read_with_args', function (done) {
+        async_test(binread.read_with_args(test_struct, test_data)(1, 2), done, function (value) {
+          assert.deepEqual(value.magic, test_result.magic);
 
-      async_test(value.version.read(), done, function (version_value) {
-        assert.equal(version_value.major, test_result.version.major + 1);
-        assert.equal(version_value.minor, test_result.version.minor + 2);
+          async_test(value.version.read(), done, function (version_value) {
+            assert.equal(version_value.major, test_result.version.major + 1);
+            assert.equal(version_value.minor, test_result.version.minor + 2);
+          });
+        }, true);
       });
-    }, true);
-  });
 
-  it('trace_read', function (done) {
-    async_test(binread.trace_read(test_struct, test_data), done, function (stack_trace) {
-      assert.equal(stack_trace.offset, 0);
-      assert.equal(stack_trace.size, 4);
-      assert.equal(stack_trace.type.func, test_struct);
-      assert.equal(stack_trace.children.length, 1);
+      it('trace_read', function (done) {
+        async_test(binread.trace_read(test_struct, test_data), done, function (stack_trace) {
+          assert.equal(stack_trace.offset, 0);
+          assert.equal(stack_trace.size, 4);
+          assert.equal(stack_trace.type.func, test_struct);
+          assert.equal(stack_trace.children.length, 1);
 
-      var magic_stack_trace = stack_trace.children[0];
-      assert.equal(magic_stack_trace.offset, 0);
-      assert.equal(magic_stack_trace.size, 4);
-      assert.deepEqual(magic_stack_trace.value, test_result.magic);
-      assert.equal(magic_stack_trace.type.func, types.bytes);
-      assert.equal(magic_stack_trace.type.args[0], 4);
+          var magic_stack_trace = stack_trace.children[0];
+          assert.equal(magic_stack_trace.offset, 0);
+          assert.equal(magic_stack_trace.size, 4);
+          assert.deepEqual(magic_stack_trace.value, test_result.magic);
+          assert.equal(magic_stack_trace.type.func, types.bytes);
+          assert.equal(magic_stack_trace.type.args[0], 4);
 
-      async_test(stack_trace.value.version.read(), done, function (version_value) {
-        assert.deepEqual(version_value, test_result.version);
+          async_test(stack_trace.value.version.read(), done, function (version_value) {
+            assert.deepEqual(version_value, test_result.version);
 
-        assert.equal(stack_trace.offset, 0);
-        assert.equal(stack_trace.size, 4);
-        assert.equal(stack_trace.type.func, test_struct);
-        assert.equal(stack_trace.children.length, 2);
+            assert.equal(stack_trace.offset, 0);
+            assert.equal(stack_trace.size, 4);
+            assert.equal(stack_trace.type.func, test_struct);
+            assert.equal(stack_trace.children.length, 2);
 
-        var magic_stack_trace_lazy = stack_trace.children[0];
-        assert.equal(magic_stack_trace_lazy.offset, 0);
-        assert.equal(magic_stack_trace_lazy.size, 4);
-        assert.deepEqual(magic_stack_trace_lazy.value, test_result.magic);
-        assert.equal(magic_stack_trace_lazy.type.func, types.bytes);
-        assert.equal(magic_stack_trace_lazy.type.args[0], 4);
+            var magic_stack_trace_lazy = stack_trace.children[0];
+            assert.equal(magic_stack_trace_lazy.offset, 0);
+            assert.equal(magic_stack_trace_lazy.size, 4);
+            assert.deepEqual(magic_stack_trace_lazy.value, test_result.magic);
+            assert.equal(magic_stack_trace_lazy.type.func, types.bytes);
+            assert.equal(magic_stack_trace_lazy.type.args[0], 4);
 
-        var version_stack_trace = stack_trace.children[1];
-        assert.equal(version_stack_trace.offset, 4);
-        assert.equal(version_stack_trace.size, 4);
-        assert.deepEqual(version_stack_trace.value, test_result.version);
-        assert.equal(version_stack_trace.type.func, test_sub_struct);
-        assert.equal(version_stack_trace.type.args[0], 0);
-        assert.equal(version_stack_trace.type.args[1], 0);
-        assert.equal(version_stack_trace.children.length, 2);
+            var version_stack_trace = stack_trace.children[1];
+            assert.equal(version_stack_trace.offset, 4);
+            assert.equal(version_stack_trace.size, 4);
+            assert.deepEqual(version_stack_trace.value, test_result.version);
+            assert.equal(version_stack_trace.type.func, test_sub_struct);
+            assert.equal(version_stack_trace.type.args[0], 0);
+            assert.equal(version_stack_trace.type.args[1], 0);
+            assert.equal(version_stack_trace.children.length, 2);
 
-        var major_stack_trace = version_stack_trace.children[0];
-        assert.equal(major_stack_trace.offset, 4);
-        assert.equal(major_stack_trace.size, 2);
-        assert.equal(major_stack_trace.value, test_result.version.major);
-        assert.equal(major_stack_trace.type.func, types.uint16);
+            var major_stack_trace = version_stack_trace.children[0];
+            assert.equal(major_stack_trace.offset, 4);
+            assert.equal(major_stack_trace.size, 2);
+            assert.equal(major_stack_trace.value, test_result.version.major);
+            assert.equal(major_stack_trace.type.func, types.uint16);
 
-        var minor_stack_trace = version_stack_trace.children[1];
-        assert.equal(minor_stack_trace.offset, 6);
-        assert.equal(minor_stack_trace.size, 2);
-        assert.equal(minor_stack_trace.value, test_result.version.minor);
-        assert.equal(minor_stack_trace.type.func, types.beint16);
+            var minor_stack_trace = version_stack_trace.children[1];
+            assert.equal(minor_stack_trace.offset, 6);
+            assert.equal(minor_stack_trace.size, 2);
+            assert.equal(minor_stack_trace.value, test_result.version.minor);
+            assert.equal(minor_stack_trace.type.func, types.beint16);
+          });
+        }, true);
       });
-    }, true);
-  });
 
-  it('trace_read_with_args', function (done) {
-    async_test(binread.trace_read_with_args(test_struct, test_data)(1, 2), done, function (stack_trace) {
-      assert.equal(stack_trace.offset, 0);
-      assert.equal(stack_trace.size, 4);
-      assert.equal(stack_trace.type.func, test_struct);
-      assert.equal(stack_trace.children.length, 1);
+      it('trace_read_with_args', function (done) {
+        async_test(binread.trace_read_with_args(test_struct, test_data)(1, 2), done, function (stack_trace) {
+          assert.equal(stack_trace.offset, 0);
+          assert.equal(stack_trace.size, 4);
+          assert.equal(stack_trace.type.func, test_struct);
+          assert.equal(stack_trace.children.length, 1);
 
-      var magic_stack_trace = stack_trace.children[0];
-      assert.equal(magic_stack_trace.offset, 0);
-      assert.equal(magic_stack_trace.size, 4);
-      assert.deepEqual(magic_stack_trace.value, test_result.magic);
-      assert.equal(magic_stack_trace.type.func, types.bytes);
-      assert.equal(magic_stack_trace.type.args[0], 4);
+          var magic_stack_trace = stack_trace.children[0];
+          assert.equal(magic_stack_trace.offset, 0);
+          assert.equal(magic_stack_trace.size, 4);
+          assert.deepEqual(magic_stack_trace.value, test_result.magic);
+          assert.equal(magic_stack_trace.type.func, types.bytes);
+          assert.equal(magic_stack_trace.type.args[0], 4);
 
-      async_test(stack_trace.value.version.read(), done, function (version_value) {
-        assert.equal(version_value.major, test_result.version.major + 1);
-        assert.equal(version_value.minor, test_result.version.minor + 2);
+          async_test(stack_trace.value.version.read(), done, function (version_value) {
+            assert.equal(version_value.major, test_result.version.major + 1);
+            assert.equal(version_value.minor, test_result.version.minor + 2);
 
-        assert.equal(stack_trace.offset, 0);
-        assert.equal(stack_trace.size, 4);
-        assert.equal(stack_trace.type.func, test_struct);
-        assert.equal(stack_trace.children.length, 2);
+            assert.equal(stack_trace.offset, 0);
+            assert.equal(stack_trace.size, 4);
+            assert.equal(stack_trace.type.func, test_struct);
+            assert.equal(stack_trace.children.length, 2);
 
-        var magic_stack_trace_lazy = stack_trace.children[0];
-        assert.equal(magic_stack_trace_lazy.offset, 0);
-        assert.equal(magic_stack_trace_lazy.size, 4);
-        assert.deepEqual(magic_stack_trace_lazy.value, test_result.magic);
-        assert.equal(magic_stack_trace_lazy.type.func, types.bytes);
-        assert.equal(magic_stack_trace_lazy.type.args[0], 4);
+            var magic_stack_trace_lazy = stack_trace.children[0];
+            assert.equal(magic_stack_trace_lazy.offset, 0);
+            assert.equal(magic_stack_trace_lazy.size, 4);
+            assert.deepEqual(magic_stack_trace_lazy.value, test_result.magic);
+            assert.equal(magic_stack_trace_lazy.type.func, types.bytes);
+            assert.equal(magic_stack_trace_lazy.type.args[0], 4);
 
-        var version_stack_trace = stack_trace.children[1];
-        assert.equal(version_stack_trace.offset, 4);
-        assert.equal(version_stack_trace.size, 4);
-        assert.equal(version_stack_trace.type.func, test_sub_struct);
-        assert.equal(version_stack_trace.type.args[0], 1);
-        assert.equal(version_stack_trace.type.args[1], 2);
-        assert.equal(version_stack_trace.children.length, 2);
+            var version_stack_trace = stack_trace.children[1];
+            assert.equal(version_stack_trace.offset, 4);
+            assert.equal(version_stack_trace.size, 4);
+            assert.equal(version_stack_trace.type.func, test_sub_struct);
+            assert.equal(version_stack_trace.type.args[0], 1);
+            assert.equal(version_stack_trace.type.args[1], 2);
+            assert.equal(version_stack_trace.children.length, 2);
 
-        var major_stack_trace = version_stack_trace.children[0];
-        assert.equal(major_stack_trace.offset, 4);
-        assert.equal(major_stack_trace.size, 2);
-        assert.equal(major_stack_trace.value, test_result.version.major);
-        assert.equal(major_stack_trace.type.func, types.uint16);
+            var major_stack_trace = version_stack_trace.children[0];
+            assert.equal(major_stack_trace.offset, 4);
+            assert.equal(major_stack_trace.size, 2);
+            assert.equal(major_stack_trace.value, test_result.version.major);
+            assert.equal(major_stack_trace.type.func, types.uint16);
 
-        var minor_stack_trace = version_stack_trace.children[1];
-        assert.equal(minor_stack_trace.offset, 6);
-        assert.equal(minor_stack_trace.size, 2);
-        assert.equal(minor_stack_trace.value, test_result.version.minor);
-        assert.equal(minor_stack_trace.type.func, types.beint16);
+            var minor_stack_trace = version_stack_trace.children[1];
+            assert.equal(minor_stack_trace.offset, 6);
+            assert.equal(minor_stack_trace.size, 2);
+            assert.equal(minor_stack_trace.value, test_result.version.minor);
+            assert.equal(minor_stack_trace.type.func, types.beint16);
+          });
+        }, true);
       });
-    }, true);
-  });
 
-  it('should correctly report errors for traces', function (done) {
-    binread.trace_read(types.int8, []).then(function (value) {
-      assert.ok(false);
-      done('Nothing thrown');
-    }).catch(function (err) {
-      assert.ok(err.err instanceof RangeError);
-      assert.equal(err.stack_traces.length, 1);
-      assert.equal(err.offset, 0);
-      done();
+      it('should correctly report errors for traces', function (done) {
+        binread.trace_read(types.int8, []).then(function (value) {
+          assert.ok(false);
+          done('Nothing thrown');
+        }).catch(function (err) {
+          assert.ok(err.err instanceof RangeError);
+          assert.equal(err.stack_traces.length, 1);
+          assert.equal(err.offset, 0);
+          done();
+        });
+      });
     });
-  });
+  };
+  tests(test_data_array, 'array');
+  tests(new Uint8Array(test_data_array).buffer, 'ArrayBuffer');
+  tests(new Buffer(test_data_array), 'Buffer');
+
+  var fd = fs.openSync('test.tmp', 'w');
+  fs.writeSync(fd, new Buffer(test_data_array), 0, test_data_array.length);
+  fs.closeSync(fd);
+
+  tests('test.tmp', 'NodeFile');
 });
--- test/sources.js
+++ test/sources.js
@@ -3,6 +3,12 @@
 
 var assert = require('assert');
 var sources = require('../lib/sources');
+var utils = require('./utils');
+var async_test = utils.async_test;
+
+var ident = function (val) {
+  return val;
+};
 
 describe('binread', function () {
   describe('.sources', function () {
@@ -12,15 +18,53 @@ describe('binread', function () {
       }, TypeError);
     });
 
-    it('must handle correctly array of bytes', function () {
+    it('must handle correctly array of bytes', function (done) {
+      var source = null;
       assert.doesNotThrow(function () {
-        sources([0x00, 0x01]);
+        source = sources([0x00, 0x01]);
+      });
+
+      assert.equal(source.size(), 2);
+
+      async_test(source.getDataView(0, 2, ident), done, function (value) {
+        assert.ok(value instanceof DataView);
+        assert.equal(value.byteOffset, 0);
+        assert.equal(value.byteLength, 2);
+        assert.equal(value.getUint8(0), 0x00);
+        assert.equal(value.getUint8(1), 0x01);
       });
     });
 
-    it('must handle correctly ArrayBuffers', function () {
+    it('must handle correctly ArrayBuffers', function (done) {
+      var source = null;
       assert.doesNotThrow(function () {
-        sources(new ArrayBuffer(2));
+        source = sources(new Uint8Array([0x00, 0x01]).buffer);
+      });
+
+      assert.equal(source.size(), 2);
+
+      async_test(source.getDataView(0, 2, ident), done, function (value) {
+        assert.ok(value instanceof DataView);
+        assert.equal(value.byteOffset, 0);
+        assert.equal(value.byteLength, 2);
+        assert.equal(value.getUint8(0), 0x00);
+        assert.equal(value.getUint8(1), 0x01);
+      });
+    });
+
+    it('must handle correctly Buffers(node)', function (done) {
+      var source = null;
+      assert.doesNotThrow(function () {
+        source = sources(new Buffer([0x00, 0x01]));
+      });
+
+      assert.equal(source.size(), 2);
+
+      async_test(source.getDataView(0, 2, ident), done, function (value) {
+        assert.equal(value.byteOffset, 0);
+        assert.equal(value.byteLength, 2);
+        assert.equal(value.getUint8(0), 0x00);
+        assert.equal(value.getUint8(1), 0x01);
       });
     });
   });
